b0f631fef837edbc3dcdc6fd90169a357d6c5907
GeekwiseNicole <geekwise.nicole.delotro@gmail.com>
2016-05-16 19:07:18 +0000

    starting off where i left off

--- wp-content/themes/html5blank-stable/about.php
+++ wp-content/themes/html5blank-stable/about.php
@@ -1,41 +1,56 @@
 <?php /* Template Name: about */ ?>
 <?php get_header('about'); ?>
 
-<div class="section_1">
-<span><img src="http://i.imgur.com/OxY5oKC.png" alt="owl"></span>
-    <li class="active"><a href="http://geekwiseacademy.com/signup">signup</a></li>
-       <h3>Learn a real world <br>technology skill. Fast.</h3>
-</div>
-     
-<section>
-    <article>
-        <p>WHAT</p>Geekwise Academy is an accelerated training program for current and aspiring technologists. Geekwise Academy teaches trainees hard skills in the technology industry in only six weeks. Each course is fast-paced, rigorous, and narrowly focused on a hard skill that is presently in-demand in the technology industry.
+ <div class="section_1">
+       <span><img src="http://i.imgur.com/OxY5oKC.png" alt="owl"></span>
+       <li class="active"><a href="signup.html">   </a></li>
+       <h3>   </h3>
+      </div>
+      <section>
+          <article>
+          <p></p>
+          
           </article>
           <article>
-          <p>HOW</p>
-            The subject matter for each course is selected by an employer who wants to recruit technologists right out of the Geekwise Academy classroom. Geekwise Academy then works with the employer to build a curriculum specifically designed to effectively teach that skill. Each course is then taught by an industry-expert lecturer. Every second of every course is designed to provide you with the skills that technology employers and clients need you to have.
+          <p> </p>
      </article>
       </section>
         
          <section>
           <article>
-           <p>WHY</p>
-            We need more geeks. Lots of them. And good ones. It’s no secret that software and information technology are among the fastest growing industries on the planet. Despite that truth, there simply aren’t enough technologists to keep up with the demand. Not to mention that these industries offer promising careers and have mountains of jobs that are available today. Geekwise Academy exists to fill that demand by rapidly producing high quality technologists, and then connecting those same technologists to real world opportunities.
+           <p>  </p>
+        
          </article>
           <article>
-             <p>DETAILS</p>
-              <p>Length:<br>Each course lasts 6 weeks</p>
-              <p>Time:<br>Each course meets from 6:00-9:00p, two nights per week</p>
-              <p>Location:<br>All Classes meet at Bitwise Mural District (2210 San Joaquin St., Fresno, CA 93721)</p>
-              <p>Cost:<br>Each class costs only $250</p>
+             <p>   </p>
+              <p>   </p>
+              <p>    </p>
+              <p>   </p>
+              <p>    </p>
          </article>
       </section>
      
     </main>
      <footer>
-         <a href="http://geekwiseacademy.com/signup"></a>
-         <div id="today">want to geekwise &#63;sign-up today</div>
+         <div id="today">    &#63;   </div>
     </footer>
     </body>
 </html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ wp-content/themes/html5blank-stable/cohort.php
@@ -0,0 +1,145 @@
+<body>
+  <main>
+   <div class="top">
+     <li class="none"><a href="index.html"><img src="http://i.imgur.com/LAA3RoI.png" alt="geekwise"></a></li>
+      <nav>
+       <ul>
+           <li><a href="about.html">    </a></li>
+           <li><a href="courses.html">   </a></li>
+           <li><a href="workshops.html">   </a></li>
+           <li class="yellow"><a href="cohort.html">    </a></li>
+           <li><a href="team.html">   </a></li>
+           <li><a href="contact.html">   </a></li>
+           <li class="active"><a href=""> </a></li>
+       </ul>
+       </nav>
+      </div>
+      <section>
+      <h2>What is The Geekwise Academy Cohort Program?</h2>
+      <p>The Geekwise Academy Cohort program has been developed to bridge the gap between Geekwise Academy course students and their ability to move into rlies as developers in the local technliogy industry. </p>
+
+      <p>The Cohort consists of two phases. The first is a two week internship for the 20 individuals that were invited to the Cohort program. This phase includes QA and testing of the invitees skill set. In phase two, 8 of the 20 Invitees will be chosen to participate in a 24 week internship program. Phase two consists of three assignments each eight weeks in length. During this time the Cohorts will complete work for clients through projects of increased difficulty. Also, Cohort participants will be required to co-teach Geekwise Academy classes. They will work with the Cohort Director and Bethany Mily to prepare for this opportunity.</p>
+
+      <p>Our goal here is simple, we want to produce developers that are:</p>
+     <ol>
+      <li class="number">Information literate</li>
+      <li class="number">Confident in their problem sliving abilities</li>
+      <li class="number">Reliable and professional</li>
+      <li class="number">Good communicators</li>
+      <li class="number">Driven to succeed, able to learn, and receive criticism</li>
+      </ol>
+        
+      </section>
+      <div>
+           <p>Bitwise Industries is excited about this new adventure that kicks off on January 12, 2015. We look forward to mentoring the next generation of technliogists in Fresno.</p>
+      </div>
+      
+      <div class="container">
+      <div class="cohorts">
+         <h4>Meet the Geekwise Academy Cohort Classes!</h4>
+          <label class="fa fa-angle-down down_angle red" for="toggle"><u> Cohort Class 2 (July 2015 - January 2016)</u></label>
+          <input type="checkbox" id="toggle">
+         <table> 
+               <tbody><tr class="group_bar">
+              <td class="class2">
+                 <img src="http://i.imgur.com/vCqotav.jpg" alt="adam" class="photos_cohort">
+                  <h1 class="name">Adam Field</h1>
+                  <p class="story">Geekwise has been my springboard into the amazing world of development. The Cohort, I feel is the next step on that road. I see it as a challenge to grow and improve my skills as a developer. I'm thrilled to see where things go</p>
+              </td>
+              <td class="class2">
+                  <img src="http://i.imgur.com/fnM2yAw.jpg" alt="name" class="photos_cohort">
+                  <h1 class="name">Patrick Patterson</h1>
+                  <p class="story">As a kid I would break all my toys just to see how everything worked inside. Now, that fascination is what keeps me learning new things and looking for exciting experiences. The Cohort is the perfect opportunity for me to pursue my true potential!</p>
+              </td>
+              <td class="class2">
+                  <img src="http://i.imgur.com/NvTPypP.jpg" alt="name" class="photos_cohort">
+                  <h1 class="name">Hani Ali</h1>
+                  <p class="story">When I was invited to the Cohort I was very excited because there is a big opportunity for me to learn, add to my experience as a computer engineer and work on different projects. My plan is to learn new computer-languages, build apps and stay positive.</p>
+              </td>
+              <td class="class2">
+                  <img src="http://i.imgur.com/CSLT436.jpg" alt="name" class="photos_cohort">
+                  <h1 class="name">Hunter Lester</h1>
+                  <p class="story">Former farm, produce, &amp;s; dairy worker, I’ve come to the Cohort to help build a community in a much more effective way than I could have in agriculture. I feel at home at Bitwise and I’m excited to build a career in a field that will continue to change throughout my lifetime.</p>
+                 </td>
+                 <td class="class2">
+                  <img src="http://i.imgur.com/j4mcOvq.jpg" alt="name" class="photos_cohort">
+                  <h1 class="name">Ian Garoian</h1>
+                  <p class="story">I know that the Cohort will provide me with the tools and knowledge to sustain a long lasting career in web development. I am very honored and humbled to have been chosen and really consider this a massive building block that will guide me through the rest of my development career. I'm also really excited that I don't have to sell cell phones ever again!</p>
+              </td>
+              <td class="class2">
+                  <img src="http://i.imgur.com/PNItmN0.jpg" alt="name" class="photos_cohort">
+                  <h1 class="name">Alex Gutierrez</h1>
+                  <p class="story">I’ve spent the last few years working as a community advocate serving underrepresented communities. I’ve always had a deep appreciation for technology and more specifically, the ability to create with the power of a computer. Being chosen for the Cohort is a blessing that I hope will give me the tools needed to build something that will help make peoples lives better.</p>
+              </td>
+              <td class="class2">
+                  <img src="http://i.imgur.com/kplKRIR.jpg" alt="name" class="photos_cohort">
+                  <h1 class="name">Raquel Garcia</h1>
+                  <p class="story">When I was invited to be in Cohort, I was overwhelmed with emotions. I was happy, but I was especially grateful for the opportunity because I have never had a community believe in me the way Bitwise has. I'm excited to see what I can accomplish by the end of the program and cannot wait to see what it helps me accomplish in the future.</p>
+              </td>
+              <td class="class2">
+                  <img src="http://i.imgur.com/v0nUtY8.jpg" alt="name" class="photos_cohort">
+                  <h1 class="name">     </h1>
+                  <p class="story">     </p>
+            
+                   </td>
+                 
+             </tr>
+ </tbody></table>
+      </div>
+                 <div class="cohorts">
+          <label class="fa fa-angle-down down_angle green" for="toggle2"><u>     </u></label>
+          <input type="checkbox" id="toggle2">
+         <table> 
+                         <tbody><tr>
+                                     
+                      <td class="class2">
+                          <img src="http://i.imgur.com/2S08cR0.jpg" alt="name" class="photos_cohort">
+                          <h1 class="name">     </h1>
+                          <p class="story">    </p>
+                      </td>
+                      <td class="class2">
+                          <img src="http://i.imgur.com/7ydkI3R.jpg" alt="name" class="photos_cohort">
+                          <h1 class="name">    </h1>
+                          <p class="story">    </p>
+                      </td>
+                      <td class="class2">
+                          <img src="http://i.imgur.com/9Z83jBM.jpg" alt="name" class="photos_cohort">
+                          <h1 class="name">    </h1>
+                          <p class="story">    </p>
+                          <html>
+                              
+                          </html></p>
+                      </td>
+                      <td class="class2">
+                          <img src="http://i.imgur.com/s1feR8v.jpg" alt="" class="photos_cohort">
+                          <h1 class="name">     </h1>
+                          <p class="story"> </p>
+                      </td>
+                      <td class="class2">
+                          <img src="http://i.imgur.com/qdejOcf.jpg" alt="name" class="photos_cohort">
+                          <h1 class="name">        </h1>
+                          <p class="story">        </p>
+                      </td>
+                      <td class="class2">
+                          <img src="http://i.imgur.com/LIH0jPA.jpg" alt="name" class="photos_cohort">
+                          <h1 class="name">     </h1>
+                          <p class="story">           </p>
+                      </td>
+                      <td class="class2">
+                          <img src="http://i.imgur.com/BzHjQDD.jpg" alt="name" class="photos_cohort">
+                          <h1 class="name">     </h1>
+                          <p class="story">      </p>
+                                        </td>
+                      <td class="class2">
+                          <img src="http://i.imgur.com/XtyYWsw.jpg" alt="name" class="photos_cohort">
+                          <h1 class="name">    </h1>
+                          <p class="story">   /p>
+                      </td>
+                 </tr>
+                     </tbody></table>
+                                     
+          </div>
+      </div>
+    </main>
+</body>
+</html>
