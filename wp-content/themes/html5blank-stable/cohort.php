50ee077f3a9636b1542046a46ac6f8e600f245e2
GeekwiseNicole <geekwise.nicole.delotro@gmail.com>
2016-05-16 19:18:57 +0000

    lunch

--- wp-content/themes/html5blank-stable/cohort.php
+++ wp-content/themes/html5blank-stable/cohort.php
@@ -15,66 +15,66 @@
        </nav>
       </div>
       <section>
-      <h2>What is The Geekwise Academy Cohort Program?</h2>
-      <p>The Geekwise Academy Cohort program has been developed to bridge the gap between Geekwise Academy course students and their ability to move into rlies as developers in the local technliogy industry. </p>
+      <h2>    </h2>
+      <p>   </p>
 
-      <p>The Cohort consists of two phases. The first is a two week internship for the 20 individuals that were invited to the Cohort program. This phase includes QA and testing of the invitees skill set. In phase two, 8 of the 20 Invitees will be chosen to participate in a 24 week internship program. Phase two consists of three assignments each eight weeks in length. During this time the Cohorts will complete work for clients through projects of increased difficulty. Also, Cohort participants will be required to co-teach Geekwise Academy classes. They will work with the Cohort Director and Bethany Mily to prepare for this opportunity.</p>
+      <p>     </p>
 
-      <p>Our goal here is simple, we want to produce developers that are:</p>
+      <p>      </p>
      <ol>
-      <li class="number">Information literate</li>
-      <li class="number">Confident in their problem sliving abilities</li>
-      <li class="number">Reliable and professional</li>
-      <li class="number">Good communicators</li>
-      <li class="number">Driven to succeed, able to learn, and receive criticism</li>
+      <li class="number">   </li>
+      <li class="number">   </li>
+      <li class="number">     </li>
+      <li class="number">    </li>
+      <li class="number">     </li>
       </ol>
         
       </section>
       <div>
-           <p>Bitwise Industries is excited about this new adventure that kicks off on January 12, 2015. We look forward to mentoring the next generation of technliogists in Fresno.</p>
+           <p>   </p>
       </div>
       
       <div class="container">
       <div class="cohorts">
-         <h4>Meet the Geekwise Academy Cohort Classes!</h4>
-          <label class="fa fa-angle-down down_angle red" for="toggle"><u> Cohort Class 2 (July 2015 - January 2016)</u></label>
+         <h4>        </h4>
+          <label class="fa fa-angle-down down_angle red" for="toggle"><u>        </u></label>
           <input type="checkbox" id="toggle">
          <table> 
                <tbody><tr class="group_bar">
               <td class="class2">
                  <img src="http://i.imgur.com/vCqotav.jpg" alt="adam" class="photos_cohort">
-                  <h1 class="name">Adam Field</h1>
-                  <p class="story">Geekwise has been my springboard into the amazing world of development. The Cohort, I feel is the next step on that road. I see it as a challenge to grow and improve my skills as a developer. I'm thrilled to see where things go</p>
+                  <h1 class="name">   </h1>
+                  <p class="story">    </p>
               </td>
               <td class="class2">
                   <img src="http://i.imgur.com/fnM2yAw.jpg" alt="name" class="photos_cohort">
-                  <h1 class="name">Patrick Patterson</h1>
-                  <p class="story">As a kid I would break all my toys just to see how everything worked inside. Now, that fascination is what keeps me learning new things and looking for exciting experiences. The Cohort is the perfect opportunity for me to pursue my true potential!</p>
+                  <h1 class="name">    </h1>
+                  <p class="story">   </p>
               </td>
               <td class="class2">
                   <img src="http://i.imgur.com/NvTPypP.jpg" alt="name" class="photos_cohort">
-                  <h1 class="name">Hani Ali</h1>
-                  <p class="story">When I was invited to the Cohort I was very excited because there is a big opportunity for me to learn, add to my experience as a computer engineer and work on different projects. My plan is to learn new computer-languages, build apps and stay positive.</p>
+                  <h1 class="name">      </h1>
+                  <p class="story">    </p>
               </td>
               <td class="class2">
                   <img src="http://i.imgur.com/CSLT436.jpg" alt="name" class="photos_cohort">
-                  <h1 class="name">Hunter Lester</h1>
-                  <p class="story">Former farm, produce, &amp;s; dairy worker, I’ve come to the Cohort to help build a community in a much more effective way than I could have in agriculture. I feel at home at Bitwise and I’m excited to build a career in a field that will continue to change throughout my lifetime.</p>
+                  <h1 class="name">     </h1>
+                  <p class="story"> </p>
                  </td>
                  <td class="class2">
                   <img src="http://i.imgur.com/j4mcOvq.jpg" alt="name" class="photos_cohort">
-                  <h1 class="name">Ian Garoian</h1>
-                  <p class="story">I know that the Cohort will provide me with the tools and knowledge to sustain a long lasting career in web development. I am very honored and humbled to have been chosen and really consider this a massive building block that will guide me through the rest of my development career. I'm also really excited that I don't have to sell cell phones ever again!</p>
+                  <h1 class="name">     </h1>
+                  <p class="story">     </p>
               </td>
               <td class="class2">
                   <img src="http://i.imgur.com/PNItmN0.jpg" alt="name" class="photos_cohort">
-                  <h1 class="name">Alex Gutierrez</h1>
-                  <p class="story">I’ve spent the last few years working as a community advocate serving underrepresented communities. I’ve always had a deep appreciation for technology and more specifically, the ability to create with the power of a computer. Being chosen for the Cohort is a blessing that I hope will give me the tools needed to build something that will help make peoples lives better.</p>
+                  <h1 class="name">   </h1>
+                  <p class="story">     </p>
               </td>
               <td class="class2">
                   <img src="http://i.imgur.com/kplKRIR.jpg" alt="name" class="photos_cohort">
-                  <h1 class="name">Raquel Garcia</h1>
-                  <p class="story">When I was invited to be in Cohort, I was overwhelmed with emotions. I was happy, but I was especially grateful for the opportunity because I have never had a community believe in me the way Bitwise has. I'm excited to see what I can accomplish by the end of the program and cannot wait to see what it helps me accomplish in the future.</p>
+                  <h1 class="name"> </h1></h1>
+                  <p class="story"> </p>
               </td>
               <td class="class2">
                   <img src="http://i.imgur.com/v0nUtY8.jpg" alt="name" class="photos_cohort">
