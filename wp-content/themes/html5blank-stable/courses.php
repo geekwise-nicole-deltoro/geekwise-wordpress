6c5cd709dd515c7cf1e75c27b66e0b050b099c9f
GeekwiseNicole <geekwise.nicole.delotro@gmail.com>
2016-05-19 18:07:01 +0000

    finished

--- wp-content/themes/html5blank-stable/courses.php
+++ wp-content/themes/html5blank-stable/courses.php
@@ -1,75 +1,393 @@
  <?php /* Template Name: courses*/ ?>
 <?php get_header('courses'); ?>
-      
- <link href="//db.onlinewebfonts.com/c/e5f2f90ef20309de329a7d174c3d2245?family=Franchise" rel="stylesheet" type="text/css"/>
+       
+  <link href="//db.onlinewebfonts.com/c/e5f2f90ef20309de329a7d174c3d2245?family=Franchise" rel="stylesheet" type="text/css"/>
 
-   
-       <div id="workshops_fluff" class="courses_fluff">
-           <p>
-           <span>NO FLUFF. WE TEACH SKILLS THAT YOU CAN USE TO MAKE MONEY.</span>
-           </p>
-       </div>
-       
-   <main id="courses_main">
-       <div>
-           <p>
-               Geekwise Academy Courses are super-fast-paced and specifically designed to ensure that you’re learning what we’re teaching. Each course begins with a crash course in “How to Geekwise,” which will give you strategies for getting every ounce of benefit you can out of your Geekwise Academy experience. Geekwise Academy Courses deploy a social learning process that is intended to provide you with workplace communication training to go alongside the technology skill you signed up for. Courses include regular assessments to evaluate your progress and to make sure no one gets left behind. Finally, each course wraps up with a final project that is actual real world work.
-           </p>
-       </div>
-       
+    
+        <div id="workshops_fluff" class="courses_fluff">
+            <p>
+            <span>NO FLUFF. WE TEACH SKILLS THAT YOU CAN USE TO MAKE MONEY.</span>
+            </p>
+        </div>
+        
+    <main id="courses_main">
+        <div>
+            <p>
+                Geekwise Academy Courses are super-fast-paced and specifically designed to ensure that you’re learning what we’re teaching. Each course begins with a crash course in “How to Geekwise,” which will give you strategies for getting every ounce of benefit you can out of your Geekwise Academy experience. Geekwise Academy Courses deploy a social learning process that is intended to provide you with workplace communication training to go alongside the technology skill you signed up for. Courses include regular assessments to evaluate your progress and to make sure no one gets left behind. Finally, each course wraps up with a final project that is actual real world work.
+            </p>
+        </div>
+        
 <!-- added this nav to make background white, which can be seen when looking at the owl background image on this page -->
-       
+        
 <nav id="white_background">
-  
+   
 <!-- table title -->
-   <section class="courses_table" id="courses_title">
-       <div class="column1" id="courses_height">
-           <p>
-               <span class="courses_bold">
-                   Topic
-               </span>
-           </p>
-       </div>
-       <div class="column2">
-           <p>
-               <span class="courses_bold">
-                   Duration
-               </span>
-           </p>
-       </div>
-       <div class="column3">
-           <p>
-               <span class="courses_bold">
-                   Day/Time
-               </span>
-           </p>
-       </div>
-       <div class="column4">
-           <p>
-               <span class="courses_bold">
-                   Fee
-               </span>
-           </p>
-       </div>
-       <div class="column5">
-           <p>
-               <span class="courses_bold">
-                   Instructor
-               </span>
-           </p>
-       </div>
-       <div class="column6">
-           <p>
-               <span class="courses_bold">
-                   Sign-up
-               </span>
-           </p>
-       </div> 
-   </section>
-       
+    <section class="courses_table" id="courses_title">
+        <div class="column1" id="courses_height">
+            <p>
+                <span class="courses_bold">
+                    Topic
+                </span>
+            </p>
+        </div>
+        <div class="column2">
+            <p>
+                <span class="courses_bold">
+                    Duration
+                </span>
+            </p>
+        </div>
+        <div class="column3">
+            <p>
+                <span class="courses_bold">
+                    Day/Time
+                </span>
+            </p>
+        </div>
+        <div class="column4">
+            <p>
+                <span class="courses_bold">
+                    Fee
+                </span>
+            </p>
+        </div>
+        <div class="column5">
+            <p>
+                <span class="courses_bold">
+                    Instructor
+                </span>
+            </p>
+        </div>
+        <div class="column6">
+            <p>
+                <span class="courses_bold">
+                    Sign-up
+                </span>
+            </p>
+        </div> 
+    </section>
+        
 <!-- table 1 -->
-  
-   <section class="courses_table courses_border_top" id="courses_table1">
-       <div class="column1">
-           <p class="courses_underline" id="courses_subtitle1">
-               <span class="sold_out">SOLD OUT &nbsp;</span>Websites for Beginners - Starts April 4, 2016
+   
+    <section class="courses_table courses_border_top" id="courses_table1">
+        <div class="column1">
+            <p class="courses_underline" id="courses_subtitle1">
+                <span class="sold_out">SOLD OUT &nbsp;</span>Websites for Beginners - Starts April 4, 2016
+            <p class="courses_padding_bottom1">
+                <i>
+                    Websites for beginners. Part I of II. Build websites in HTML5, CSS3. Learn industry tools, page construction, industry specific terminology.
+                </i>
+            </p>
+        </div>
+        <div class="column2">
+            <p>
+                <span class="courses_bold courses_position_top">
+                    6 Weeks
+                </span>
+            </p>
+            <p class="courses_font_weight">
+                April 4 thru
+            </p>
+            <p class="courses_font_weight">
+                May 11
+            </p>
+        </div>
+        <div class="column3 courses_column_Monday">
+            <p>
+                <span  class="courses_bold">
+                    Monday and Wednesday
+                </span>
+            </p>
+            <p  class="courses_padding_bottom2">
+                6:00p - 9:00p
+            </p>
+        </div>
+        <div class="column4">
+            <p>
+                <span class="courses_bold">
+                    $250
+                </span>
+            </p>
+            <p  class="courses_padding_bottom2">
+                36 hrs. in-person instruction
+            </p>
+        </div>
+        <div class="column5">
+            <p class="courses_padding_bottom3">
+                TBD
+            </p>
+        </div>
+        <div class="column6">
+            <p class="courses_enroll">
+                <a href="#" class="enroll_anchor">
+                    <span>
+                        ENROLL
+                    </span>
+                </a>
+            </p>
+        </div>
+    </section>
+        
+<!-- table 2 -->
+       
+    <section class="courses_table courses_border_top" id="courses_table2">
+        <div class="column1">
+            <p class="courses_underline" id="courses_subtitle2">
+                <span class="sold_out">SOLD OUT &nbsp;</span>Javascript for Beginners - Starts April 4, 2016
+            </p>
+            <p class="courses_padding_bottom1">
+                <i>
+                    Javascript for beginners. Learn how to think like a programmer. Build interactive webpages in Javascript.
+                </i>
+            </p>
+        </div>
+        
+<!-- need to redo these columns because found out when doing media query some were in one p tag instead of separated ones for each line -->
+        
+        <div class="column2">
+            <p>
+                <span class="courses_bold courses_position_top">
+                    6 Weeks
+                </span>
+            </p>
+            <p>
+                April 4 thru
+            </p>
+            <p>
+                May 11
+            </p>
+        </div>
+        <div class="column3 courses_column_Monday">
+            <p>
+                <span  class="courses_bold">
+                    Monday and Wednesday
+                </span>
+            </p>
+            <p  class="courses_padding_bottom2">
+                6:00p - 9:00p
+            </p>
+        </div>
+        <div class="column4">
+            <p>
+                <span class="courses_bold">
+                    $250
+                </span>
+            </p>
+            <p  class="courses_padding_bottom2">
+                36 hrs. in-person instruction
+            </p>
+        </div>
+        <div class="column5">
+            <p class="courses_padding_bottom3">
+                TBD
+            </p>
+        </div>
+        <div class="column6">
+            <p class="courses_enroll">
+                <a href="#" class="enroll_anchor">
+                    <span>
+                        ENROLL
+                    </span>
+                </a>
+            </p>
+        </div>
+        </section>
+        
+<!-- table 3 -->
+        <section class="courses_table courses_border_top">
+        <div class="column1">
+            <p class="courses_underline" id="courses_subtitle3">
+                Database Design - Starts April 4, 2016
+            </p>
+            <p class="courses_padding_bottom1">
+                <i>
+                    For those with intermediate Excel experience. Design and build SQL databases. Schema creation and modification, stored procedures, and data access control.
+                </i>
+            </p>
+        </div>
+        <div class="column2">
+            <p>
+                <span class="courses_bold courses_position_top">
+                    6 Weeks
+                </span>
+            </p>
+            <p>
+                April 4 thru
+            </p>
+            <p>
+                May 11
+            </p>
+        </div>
+        <div class="column3 courses_column_Monday">
+            <p>
+                <span  class="courses_bold">
+                    Monday and Wednesday
+                </span>
+            </p>
+            <p  class="courses_padding_bottom2">
+                6:00p - 9:00p
+            </p>
+        </div>
+        <div class="column4">
+            <p>
+                <span class="courses_bold">
+                    $250
+                </span>
+            </p>
+            <p  class="courses_padding_bottom2">
+                36 hrs. in-person instruction
+            </p>
+        </div>
+        <div class="column5">
+            <p class="courses_padding_bottom3">
+                TBD
+            </p>
+        </div>
+        <div class="column6">
+            <p class="courses_enroll">
+                <a href="#" class="enroll_anchor">
+                    <span>
+                        ENROLL
+                    </span>
+                </a>
+            </p>
+        </div>
+        </section>
+        
+<!-- table 4 -->
+        <section class="courses_table courses_border_top">
+        <div class="column1">
+            <p class="courses_underline" id="courses_subtitle4">
+                Mobile-First Websites - Starts April 5, 2016
+            </p>
+            <p class="courses_padding_bottom1">
+                <i>
+                    Building on Websites for Beginners, Part II of II. Build responsive websites. Learn responsive web concepts, CSS frameworks, advanced layout techniques with Flexbox and introduction to Photoshop.
+                </i>
+            </p>
+        </div>
+        <div class="column2">
+            <p>
+                <span class="courses_bold courses_position_top">
+                    6 Weeks
+                </span>
+            </p>
+            <p>
+                April 4 thru
+            </p>
+            <p>
+                May 11
+            </p>
+        </div>
+        <div class="column3 courses_column_Monday">
+            <p>
+                <span  class="courses_bold">
+                    Monday and Wednesday
+                </span>
+            </p>
+            <p class="courses_padding_bottom2">
+                6:00p - 9:00p
+            </p>
+        </div>
+        <div class="column4">
+            <p>
+                <span class="courses_bold">
+                    $250
+                </span>
+            </p>
+            <p  class="courses_padding_bottom2">
+                36 hrs. in-person instruction
+            </p>
+        </div>
+        <div class="column5">
+            <p class="courses_padding_bottom3">
+                TBD
+            </p>
+        </div>
+        <div class="column6">
+            <p class="courses_enroll">
+                <a href="#" class="enroll_anchor">
+                    <span>
+                        ENROLL
+                    </span>
+                </a>
+            </p>
+        </div>
+        </section>
+<!-- table 5 -->
+        <section class="courses_table courses_border_top courses_border_bottom">
+        <div class="column1">
+            <p class="courses_underline" id="courses_subtitle5">
+                Intro to AngularJS - Starts April 5, 2016
+            </p>
+            <p class="courses_padding_bottom1">
+                <span>
+                    <i>
+                        For intermediate Javascript programmers. Build applications using the AngularJS framework.
+                    </i>
+                </span>
+            </p>
+        </div>
+        <div class="column2">
+            <p>
+                <span class="courses_bold courses_position_top">
+                    6 Weeks
+                </span>
+            </p>
+            <p>
+                April 4 thru
+            </p>
+            <p>
+                May 11
+            </p>
+        </div>
+        <div class="column3 courses_column_Monday">
+            <p>
+                <span  class="courses_bold">
+                    Monday and Wednesday
+                </span>
+            </p>
+            <p class="courses_padding_bottom2">
+                6:00p - 9:00p
+            </p>
+        </div>
+        <div class="column4">
+            <p>
+                <span class="courses_bold">
+                    $250
+                </span>
+            </p>
+            <p  class="courses_padding_bottom2">
+                36 hrs. in-person instruction
+            </p>
+        </div>
+        <div class="column5">
+            <p class="courses_padding_bottom3">
+                TBD
+            </p>
+        </div>
+        <div class="column6">
+            <p class="courses_enroll">
+                <a href="#" class="enroll_anchor">
+                    <span>
+                        ENROLL
+                    </span>
+                </a>
+            </p>
+        </div>
+       </section>
+      </nav>
+    </main>
+    <footer class="workshops_footer">
+        <div>
+            <p>
+               <a href="/index.php?page_id=16">
+                   <span>
+                       SEATING IS LIMITED, SIGN UP TODAY
+                    </span>
+               </a>                
+            </p>
+        </div>
+    </footer>
+    <img src="http://i.imgur.com/Bkba1Pj.png" id="half_owl">
+</body>
